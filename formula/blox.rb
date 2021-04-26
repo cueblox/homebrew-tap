57b22ab2ebdfe49d600831eb3bb544d908df7f42
CueBlox <support@cueblox.com>
2021-04-26 11:50:15 +0100

    Brew formula update for blox version v0.4.0

--- formula/blox.rb
+++ formula/blox.rb
@@ -5,25 +5,25 @@
 class Blox < Formula
   desc "CueBlox"
   homepage "https://github.com/cueblox/"
-  version "0.3.2"
+  version "0.4.0"
   license "MIT"
   bottle :unneeded
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/cueblox/blox/releases/download/v0.3.2/blox_0.3.2_darwin_amd64.tar.gz"
-    sha256 "455f5d601721acb5c0c10aea35f03ee3b38fb49a978d417941994d766327ca18"
+    url "https://github.com/cueblox/blox/releases/download/v0.4.0/blox_0.4.0_darwin_amd64.tar.gz"
+    sha256 "bf76e73f05c982edcb362a740b94cdd6f9cdc2f9013c53bca750e8e983a4938d"
   end
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/cueblox/blox/releases/download/v0.3.2/blox_0.3.2_darwin_arm64.tar.gz"
-    sha256 "cf8d6907616eac932b0047418d97fd171c83db5508ee4ce032b7606c8a2fbdf6"
+    url "https://github.com/cueblox/blox/releases/download/v0.4.0/blox_0.4.0_darwin_arm64.tar.gz"
+    sha256 "f30e04796dd80c3fdad0152ff44150141e023262acabc63fb755d30ca55fc6fb"
   end
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/cueblox/blox/releases/download/v0.3.2/blox_0.3.2_linux_amd64.tar.gz"
-    sha256 "098d1164c7fe0dfc45dc32b1e9a6ba2591ea999a0e6426686227f8314d3b4aed"
+    url "https://github.com/cueblox/blox/releases/download/v0.4.0/blox_0.4.0_linux_amd64.tar.gz"
+    sha256 "3868cb8f7b2821aa815550b94d5c12fef65afa5a51c180c0e8c6f4535f432e49"
   end
   if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/cueblox/blox/releases/download/v0.3.2/blox_0.3.2_linux_arm64.tar.gz"
-    sha256 "454d7e1e0bc87c7ccea67a3e6e01392f18463e766232e4c3bd769076ab661efe"
+    url "https://github.com/cueblox/blox/releases/download/v0.4.0/blox_0.4.0_linux_arm64.tar.gz"
+    sha256 "31b0314940a5f4fd97483cbfb763db90315977eab9abd1556a89f202652345a4"
   end
 
   def install
